7ed95685d642d4e351ec7673dcd6f53c8d7a1bea
Sudiptaguru <64124103+Sudiptaguru@users.noreply.github.com>
2022-06-05 22:37:59 +0530

    Add files via upload

---
+++ database/migrations/2022_06_04_025054_create_companies_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('company_email')->unique();
+            $table->string('logo');
+            $table->string('website');
+            $table->timestamps();
+        });
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('employee_email');
+            $table->string('phone');
+            $table->bigInteger('company_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+        Schema::dropIfExists('employees');
+    }
+}
